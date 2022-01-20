96b45cd89a2708bbca434219d9c55b3a3e884728
eugene <sevaleugene@gmail.com>
2022-01-20 13:26:21 +0200

    Initial commit.

--- less2.py
+++ less2.py
@@ -1,10 +1,10 @@
+import operator 
 i=5
 a=[]
 while i >= 1:
     name = input("input num - ")
     a += name
     i -= 1
-for e in a:
-    if int(e) < 5:
-        print(e)
-print([e for e in a if int(e) > 5])
+
+print(res + '\n')
+print(res2 + '\n')
